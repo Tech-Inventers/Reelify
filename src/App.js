220caa5e27e4bbab712d6c87adb4caf737aaad99
Mnelisi-Writes <mpontshanemnelisi2002@gmail.com>
2025-04-12 20:02:16 +0200

    Mnelisi testing commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div class="names-container">
           <p className='names'>Kutullo Letageng</p>
           <p className='names'>Sharleen </p>
-          <p className='names'>Mnelisi Mpontshane </p>
+          <p className='names'>Mnelisi Mpontshane React </p>
         </div>
         
         <a
