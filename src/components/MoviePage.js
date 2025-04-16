77c74553c818dc5c633cd07cb9188ec6708d81fe
Mnelisi-Writes <mpontshanemnelisi2002@gmail.com>
2025-04-16 15:00:56 +0200

    Add MoviePage component layout and header section

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,3 +1,34 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
+function MoviePage({
+    viewMode,
+    setViewMode,
+    timeWindow,
+    setTimeWindow,
+    categories,
+    changeCategory,
+    movies,
+    handleMovieClick,
+    handleTrailerRequest,
+    isLoading,
+    currentPage,
+    totalPages,
+    handlePageChange,
+    getViewModeTitle,
+    filterYear,
+    setFilterYear,
+    filterCountry,
+    setFilterCountry,
+    sortBy,
+    setSortBy,
+    countries,
+    years,
+    resetFilters
+  }) {
+    return (
+      <div className="movie-page">
+        <section className="movie-page-header">
+          <h1>Movies</h1>
+          <p>Discover the best films from around the world</p>
+        </section>
