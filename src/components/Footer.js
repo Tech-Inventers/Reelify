4b4cb52517b07b33bd746ed4bcb943d30706fb90
Mnelisi-Writes <mpontshanemnelisi2002@gmail.com>
2025-04-17 14:06:49 +0200

    Add streaming service logo imports for Netflix, Disney+, Prime Video, and Apple TV

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+import netflixLogo from "../assests/Netflix logo.png"; 
+import disneyLogo from "../assests/disney.png";
+import primeVideoLogo from "../assests/Prime video logo.png";
+import appleTvLogo from "../assests/apple tv logo.png";
